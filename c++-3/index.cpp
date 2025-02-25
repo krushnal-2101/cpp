572851d1ed7f77cf66a4e22c169b31fb600b3152
krushnal-2101 <krushnalbhatti71@gmail.com>
2025-02-25 13:59:32 +0530

    Add files via upload

---
+++ c++-3/index.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+
+    // int x=10;
+    // int y=20;
+    // int z=30;
+
+
+    // relational opertars
+    
+    // x < y ; true
+    // x > y ; false
+    // x == y ; false
+    // x <= y ; 
+    // x >= y;
+    // x != y ;
+
+
+    // //  logical opertars
+
+    // 10 == 20 &&  20 == 10 ; false
+    // 10 == 20 || 20 == 20 ; true
+    // 10 == 20 || 20 == 30 ; true
+
+
+    // x < y  &&  y < z ;   
+    // x < y  &&  y < z ;   
+
+
+
+    // assignment operators
+
+
+    // z = x+y ;
+
+    // int a=100;
+    // int b=70;
+
+    // b = a+b;
+    //  b+=a;
+
+
+    //  cout << sizeof(b) << endl;
+
+
+
+     int n=20;
+     int n2=20;
+
+     n++;
+     n++;
+     n++;
+
+     --n2;
+     --n2;
+     --n2;
+
+
+     cout << --n2 << endl;
+     cout << --n << endl;
+    //  cout << --num2 << endl;
+
+
+    //  cout << n << "" << n2 << endl;
+
+     return 0;
+}
