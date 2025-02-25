9c3615e7e8eaab2806c72392ce12f44d2338bc57
krushnal-2101 <krushnalbhatti71@gmail.com>
2025-02-25 13:59:15 +0530

    Add files via upload

---
+++ c++-2/const.cpp
@@ -0,0 +1,16 @@
+    #include<iostream>
+     
+    #define GRID 5456
+
+    using namespace std;
+
+    int main(){
+
+       const int BOX = 1000;
+
+       cout << BOX ;
+
+
+
+       return 0;
+    }
