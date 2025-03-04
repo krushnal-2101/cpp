796f27662a10fc08cc7092a4e71359b54a7e4f03
krushnal-2101 <krushnalbhatti71@gmail.com>
2025-03-04 13:06:56 +0530

    ok

---
+++ c++-8/index.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+
+    int num = 56984;
+
+    int sum = 0;
+
+    while(num > 0)
+    {
+        sum = sum + (num % 10);
+
+        num = num/10;
+
+    }
+
+    cout << sum <<  endl;
+
+    return 0;
+}
---
+++ c++-8/sum-first.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+    
+    int num = 46897;
+
+    int  last_digit = num%10;
+
+    int first_digit = 0;
+
+    while(num > 10)
+    {
+        num = num/10;
+
+        first_digit =  num;
+        
+    }
+
+    int sum = first_digit + last_digit;
+
+    cout << sum << endl;
+
+    return 0;
+
+
+}
