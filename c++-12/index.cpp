2bdf6ac6f352b30acd4d7037b785d5f84edf6c28
krushnal-2101 <krushnalbhatti71@gmail.com>
2025-03-12 13:08:01 +0530

    erray size

---
+++ c++-12/index.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+//   int arr[5];
+
+//   cout << "arr[0] :" ;
+//   cin >> arr[0];
+
+//   cout << "arr[1] :" ;
+//   cin >> arr[1];
+
+//   cout << "arr[2] :" ;
+//   cin >> arr[2];
+
+//   cout << "arr[3] :" ;
+//   cin >> arr[3];
+
+//   cout << "arr[4] :" ;
+//   cin >> arr[4];
+
+  
+//  for(int i=0; i<5; i++)
+//  {
+//     cout <<  arr[i] <<  " ";
+//  }
+
+
+
+
+
+int size;
+cout << "size:";
+cin >> size;
+
+int arr[size];
+
+for(int i = 0; i < size; i++)
+{
+    cout <<  "arr["<< i << "]: ";
+    cin >>  arr[i];
+}
+
+for(int i=0; i<size; i++)
+{
+    cout <<  arr[i] << " ";
+}
+
+    return 0;
+}
