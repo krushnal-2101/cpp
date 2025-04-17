75bca4014c8473b0a3e1320b08db0be51e9c703a
krushnal-2101 <krushnalbhatti71@gmail.com>
2025-04-17 12:50:33 +0530

    ok

---
+++ c++-24/index.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+
+class animal{
+
+    public:
+    char name[100];
+    char breed[100];
+    int age ;
+    char type[100];
+
+
+    animal(){
+        strcpy(this->name,"-");
+        strcpy(this->breed,"-");
+        this->age = 0;
+        strcpy(this->type,"dog");
+        cout <<  "dogieeeeee" <<  endl;
+    }
+
+    animal(char n)
+    {
+        strcpy(this->type, n);
+    }
+
+};
+
+
+int main(){
+
+     animal dog1,cheetah("run"),lion("king"),whale("fish");
+
+
+     cout <<  dog1.type <<  endl;
+     cout <<  cheetah.type <<  endl;
+     cout <<  lion.type <<  endl;
+     cout << whale.type <<  endl;  
+
+    return 0;
+}
